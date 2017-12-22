f4f5cbc0b5019d719cec3a24cbe38721eca67b9a
osramywj <1027612662@qq.com>
2017-12-22 17:03:14 +0800

    1222

--- 前端/jqueryForm插件的使用/act.php
+++ 前端/jqueryForm插件的使用/act.php
@@ -2,10 +2,15 @@
 
 
 if($_FILES &&$_POST){
-
-    /*$data['status'] = '1';
+//if($_POST['username']=='yang'){
+    $data['status'] = 'success';
     $data['info'] = '成功';
-    echo json_encode($data);*/
-        echo json_encode('提交成功');
+//    echo json_encode($data);
+        echo json_encode($data);
 //    echo '提交成功';
+}else{
+    $data['status'] = 'fail';
+    $data['info'] = '失败';
+//    echo json_encode('提交失败');
+    echo json_encode($data);
 }
