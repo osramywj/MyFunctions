3cce4f557b1bb0865dcc508a9f55272a7ba91a00
yangwenju <yangwenju@iyunxiao.com>
2021-07-15 22:32:06 +0800

    remove some file

--- 实用类/导出excel/php导入导出excel实例/phpexcel_upload/dbconfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbconfig = array(
-   'host'=>'localhost',
-    'port'=>3306,
-    'username'=>'root',
-    'password'=>'123456',
-    'charset'=>'utf8',
-    'dbname'=>'web1'
-);
--- 实用类/导出excel/php导入导出excel实例/thinkphp导入导出excel实例/example/excel/Application/Common/Conf/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-    "DB_NAME" =>'test',
-    "DB_TYPE" =>'Mysql',
-    "DB_USER" =>'root',
-    "DB_PWD"  =>'root',
-    "DB_HOST" =>'localhost',
-    "DB_PORT" =>'3306'
-);
